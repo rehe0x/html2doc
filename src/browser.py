b313b7343368f9fdacb9aa6a70248176df8a869e
xiech <xiechanghong@everimaging.com>
2019-04-11 22:59:00 +0800

    update

--- src/browser.py
+++ src/browser.py
@@ -11,6 +11,9 @@ class Browser:
         #browser = webdriver.Chrome()
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
     
     def generate_content(self,docid):
